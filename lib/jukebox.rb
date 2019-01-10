2855450cddc43c6c39b2cdbc2cae788810bb124d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-10 00:53:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,7 +23,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
   songs.each_with_index do |song, i|
-    if input == song || i+1
+    if input == song || input == i+1.to_s
       puts "Playing #{song}"
     else
       puts "Invalid input, please try again"
