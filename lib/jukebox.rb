9bca08749d5f4b6fd728469116c9db0e0dccecff
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-10 00:42:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,7 +47,7 @@ def run
     puts "Please enter a command:"
     input = gets.chomp
     if input == "play"
-      play
+      play(songs)
     elsif input == "list"
       list
     elsif input == "help"
